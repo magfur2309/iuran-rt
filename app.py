0ef97fb703da3374160463b20a1df44a3c36a1f5
magfur2309 <magfurgezo@gmail.com>
2025-06-18 15:17:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -49,6 +49,8 @@ if not st.session_state.login:
             st.experimental_rerun()
         else:
             st.error("Username atau password salah.")
+        # ⛔ Tambahkan ini agar aplikasi berhenti jika belum login
+    st.stop()
 else:
     st.sidebar.write(f"👤 Login sebagai: `{st.session_state.username}` ({st.session_state.role})")
     if st.sidebar.button("Logout"):
